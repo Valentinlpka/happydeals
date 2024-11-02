c3587dd0ae7e591e9695249803a98336ee536996
Valentinlpka <Valentinlpka@users.noreply.github.com>
2024-11-02 10:41:50 +0000

    deploy: 028bf5f81227be281f7f5c8fbb4979fe241107f0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "592454250"
+    serviceWorkerVersion: "2926968941"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/AssetManifest.json": "1bfbce6d50599332c555fbe02bee8a03",
 "assets/AssetManifest.bin.json": "b7b57ee5a2d7586061e5274de35e68cf",
 "stripe.html": "cbcab0e6d6e559e5a7202820be039fcc",
-"index.html": "8e314b565cebff46e4155a0bfd1035d2",
-"/": "8e314b565cebff46e4155a0bfd1035d2",
+"index.html": "de7b1f5820017659454229b71b4afd21",
+"/": "de7b1f5820017659454229b71b4afd21",
 "manifest.json": "e92b458d83003bbe8f407e46300f8182",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -45,7 +45,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-192.png": "7418f7e58210cdcba2f3c298d08c8ef8",
 "icons/Icon-512.png": "b89118f035f8161b5fa5a0cb468d2e87",
 "version.json": "a7fea63afa0364418817d8246079537a",
-"flutter_bootstrap.js": "234746e8db0127734e7662d79d569046"};
+"flutter_bootstrap.js": "015549e3b357c979ce23f03038a826d6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
