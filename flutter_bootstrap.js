3349e0a1f3a72c25eaa073902fe95f8512563234
Valentinlpka <Valentinlpka@users.noreply.github.com>
2024-11-02 09:21:59 +0000

    deploy: aceb47a7007da3065db3f09e88f20e3293cb65e7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3592574357"
+    serviceWorkerVersion: "3070150210"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "79a01a831134469dfef13bc6ae9a0acb",
+"main.dart.js": "ca76a60b9a69cd9c49d98b340905c252",
 "assets/FontManifest.json": "429f2722e17bfc7afe286c39e2af8039",
 "assets/AssetManifest.bin": "f76d1503b17ae35c92de8f21a9454372",
 "assets/fonts/MaterialIcons-Regular.otf": "49bdc23d5a2dc42627b9762b8c2864c5",
@@ -45,7 +45,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-512.png": "b89118f035f8161b5fa5a0cb468d2e87",
 "icons/favicon.ico": "4706cf0a06ab177cefce12ae42876d44",
 "version.json": "a7fea63afa0364418817d8246079537a",
-"flutter_bootstrap.js": "d3159ef9d3e24d3d64c3c68f37c5a9a5"};
+"flutter_bootstrap.js": "f03a80d61756b2c65ac9c7d5e674476f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
