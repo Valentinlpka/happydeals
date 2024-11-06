4794c565c895983242a3aae9b743562dfb2ccc27
Valentinlpka <Valentinlpka@users.noreply.github.com>
2024-11-06 09:13:40 +0000

    deploy: d95610db6789db52068c22ecb8c4c632a695ed70

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1408640797"
+    serviceWorkerVersion: "756399395"
   }
 });
