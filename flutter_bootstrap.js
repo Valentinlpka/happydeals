6cfa2ab56f79b9cad0bf47134d936eddcfc58da7
Valentinlpka <Valentinlpka@users.noreply.github.com>
2024-11-25 08:46:37 +0000

    deploy: 09d20224a3b9e3ba950c0ed9c93a311d4b635b02

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3250894415"
+    serviceWorkerVersion: "2413161608"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "3cfd631d152b0f052a8725cbd9dc3810",
+"main.dart.js": "97d8660a32927191931dd94da686c817",
 "assets/FontManifest.json": "ea9dc826036577df024fd0fe176dd298",
 "assets/AssetManifest.bin": "a94fceb5b245bf14fdbb18540d1e6910",
 "assets/fonts/MaterialIcons-Regular.otf": "a6c3e5389ef63c3ae382d37ce629999c",
@@ -46,7 +46,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-512.png": "b89118f035f8161b5fa5a0cb468d2e87",
 "404.html": "ab579ad9dc729d0d05bd8c606381e9b0",
 "version.json": "a7fea63afa0364418817d8246079537a",
-"flutter_bootstrap.js": "3906123bcc0ed942b8eab96dc9255188"};
+"flutter_bootstrap.js": "90fdc0bc387b1472d7d1dab17e50dfa0"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
