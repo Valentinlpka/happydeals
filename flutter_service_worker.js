287c078f4776b91c13427b0d8bcac5429e36b09c
Valentinlpka <Valentinlpka@users.noreply.github.com>
2024-11-27 09:29:21 +0000

    deploy: f4a935b9037b04063c26c90e1f174c8c3d05db5f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,10 +5,10 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"service-worker.js": "0559e8cb0efdcd63c913ec4fab530605",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "ff2e0f008b3e29a90ffe468b5085b6b4",
+"main.dart.js": "fb187fe40fcc179e1bce2b4f24722b19",
 "assets/FontManifest.json": "ea9dc826036577df024fd0fe176dd298",
 "assets/AssetManifest.bin": "e6f2299f75b64456953e8be2f450b717",
-"assets/fonts/MaterialIcons-Regular.otf": "7ffc8dc65870da7668f1084e96fd289b",
+"assets/fonts/MaterialIcons-Regular.otf": "949cae5153199547d08aa1ba4dc504eb",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/underline.png": "c94a4441e753e4744e2857f0c4359bf0",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/fonts/RobotoMono-Regular.ttf": "5b04fdfec4c8c36e8ca574e40b7148bb",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/squiggly.png": "c9602bfd4aa99590ca66ce212099885f",
@@ -48,7 +48,7 @@ const RESOURCES = {"service-worker.js": "0559e8cb0efdcd63c913ec4fab530605",
 "icons/Icon-512.png": "b89118f035f8161b5fa5a0cb468d2e87",
 "404.html": "ab579ad9dc729d0d05bd8c606381e9b0",
 "version.json": "a7fea63afa0364418817d8246079537a",
-"flutter_bootstrap.js": "d0b1d98c4e249a57f7d7b9c892902996"};
+"flutter_bootstrap.js": "9c9d79480391b7fc6ab938c274904e42"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
